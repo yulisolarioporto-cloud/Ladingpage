6705b2267759218ba7b6a08d8ca3b42a0a2c6758
Yuli Solari <yuliana.solari.oporto@Gmail.com>
2026-04-17 15:13:48 -0500

    Corregir el mensaje y codigo

--- script.js
+++ script.js
@@ -1,30 +1,28 @@
 document.addEventListener("DOMContentLoaded", function () {
 
-
-mensaje.textContent = "🎬 ¡Bienvenid@ a Cartelera 2026!";
-mensaje.style.opacity = "1";
-
-setTimeout(() => {
-    mensaje.style.opacity = "0";
-}, 3000);
     const mensaje = document.getElementById("mensaje");
-    // NAV sin alert molesto
-    const categorias = document.querySelectorAll(".categories a");
 
-    categorias.forEach(function (categoria) {
+    mensaje.textContent = "🎬 ¡Bienvenid@ a Cartelera 2026!";
+    mensaje.style.opacity = "1";
+
+    setTimeout(() => {
+        mensaje.style.opacity = "0";
+    }, 3000);
+
+    // NAV
+    document.querySelectorAll(".categories a").forEach(function (categoria) {
         categoria.addEventListener("click", function () {
             mensaje.textContent = "📌 Estás viendo la categoría: " + categoria.textContent;
+            mensaje.style.opacity = "1";
         });
     });
 
-    // Click en películas
-    const peliculas = document.querySelectorAll(".movie-card");
-
-    peliculas.forEach(function (pelicula) {
+    document.querySelectorAll(".movie-card").forEach(function (pelicula) {
 
         pelicula.addEventListener("click", function () {
             const titulo = pelicula.querySelector(".title-box").textContent;
             mensaje.textContent = "🎥 Seleccionaste: " + titulo;
+            mensaje.style.opacity = "1";
         });
 
         pelicula.addEventListener("mouseover", function () {
@@ -36,7 +34,6 @@ setTimeout(() => {
         });
     });
 
-    // FORMULARIO
     const form = document.getElementById("formularioUsuario");
 
     form.addEventListener("submit", function (e) {
@@ -44,24 +41,14 @@ setTimeout(() => {
 
         const nombre = document.getElementById("nombre").value;
 
-mensaje.textContent = "✅ Gracias " + nombre + ", te has registrado correctamente";
-mensaje.style.opacity = "1";
+        mensaje.textContent = "✅ Gracias " + nombre + ", te has registrado correctamente";
+        mensaje.style.opacity = "1";
 
-setTimeout(() => {
-    mensaje.style.opacity = "0";
-}, 3000);
+        setTimeout(() => {
+            mensaje.style.opacity = "0";
+        }, 3000);
+
         form.reset();
     });
 
-});
-const form = document.getElementById("formularioUsuario");
-
-form.addEventListener("submit", function(e) {
-  e.preventDefault();
-
-
-  alert("¡Gracias por tu recomendación! 🎬");
-
-  
-  form.reset();
 });
